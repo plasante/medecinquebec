ced108873aa816ceb98ed6f20be4604bb5f75a60
Pierre Lasante <pierre.lasante@videotron.ca>
2011-12-03 13:52:22 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Medecinquebec::Application.config.secret_token = '2c2836e2a27b3c00060574244ca2540b6519056c7c2f9180ba5ae6c39ef4bb7aec30cdb7bb790d7980c7e86493a0cd28a781d09cac1bb5969a1671484b24b1ad'
